d82fcc83e4c0c41f532cd666de1c8b598d449a83
wooil <wooilkim9@gmail.com>
2021-07-10 22:36:24 +0900

    Add giveCorrds for magnet mode

--- js/main.js
+++ js/main.js
@@ -7,7 +7,8 @@ const yAxis = gridContainer.querySelector(".y-axis");
 const tileWidth = document.querySelector(".tile-width");
 const tileHeight = document.querySelector(".tile-height");
 const tileContainer = document.querySelector(".tile-container");
-const gridOn = document.querySelector(".grid-on");
+const gridMode = document.querySelector(".grid-mode");
+const magnetMode = document.querySelector(".magnet-mode");
 const testField = document.querySelector("#test-field");
 
 const MAX_GRID_SIZE = 100;
@@ -31,7 +32,8 @@ const tileOffset = {
 };
 
 let tileCount = 0;
-let isGridOn = gridOn.checked;
+let isGridMode = gridMode.checked;
+let isMagnetMode = magnetMode.checked;
 
 // functions
 
@@ -56,10 +58,16 @@ function setUnits(cont, size) {
     cont.style.height = `${height * UNIT_SIZE}rem`;
     cont.append(document.createElement("div"));
     cont.append(document.createElement("div"));
-    [{line: cont.firstChild, repeat: height}, {line: cont.lastChild, repeat: width}].forEach((item, idx) => {
+    [{
+        line: cont.firstChild,
+        repeat: height
+    }, {
+        line: cont.lastChild,
+        repeat: width
+    }].forEach((item, idx) => {
         item.line.classList.add(`${idx ? "col" : "row"}`);
         item.line.style.pointerEvents = "none";
-        for(let i = 0; i < item.repeat; i++){
+        for (let i = 0; i < item.repeat; i++) {
             const div = document.createElement("div");
             div.style.pointerEvents = "none";
             div.setAttribute("index", i);
@@ -76,13 +84,19 @@ function setUnits(cont, size) {
     })
 }
 
-function setGrid(){
+function setGrid() {
     setUnits(grid, gridSize);
     gridContainer.style.gridTemplateColumns = `1fr ${gridSize.width}fr`;
     Object.values(tileContainer.children).forEach(item => anchorTile(item));
-    [{axis: xAxis, repeat: gridSize.width}, {axis: yAxis, repeat: gridSize.height}].forEach((item, idx) => {
+    [{
+        axis: xAxis,
+        repeat: gridSize.width
+    }, {
+        axis: yAxis,
+        repeat: gridSize.height
+    }].forEach((item, idx) => {
         item.axis.innerHTML = "";
-        for(let i = 0; i < item.repeat; i++){
+        for (let i = 0; i < item.repeat; i++) {
             const div = document.createElement("div");
             div.setAttribute("index", i);
             div.innerText = i;
@@ -95,15 +109,88 @@ function setGrid(){
 function isInGrid(tile) {
     const left = tile.offsetLeft;
     const top = tile.offsetTop;
-    return !(left + HALF_UNIT_IN_PX < grid.offsetLeft || top + HALF_UNIT_IN_PX < grid.offsetTop || left + tile.offsetWidth - HALF_UNIT_IN_PX > grid.offsetLeft + grid.offsetWidth || top + tile.offsetHeight - HALF_UNIT_IN_PX > grid.offsetTop + grid.offsetHeight);
+    if (isMagnetMode) {
+        return !(left + tile.offsetWidth < grid.offsetLeft || top + tile.offsetHeight < grid.offsetTop || left > grid.offsetLeft + grid.offsetWidth || top > grid.offsetTop + grid.offsetHeight);
+    } else {
+        return !(left + HALF_UNIT_IN_PX < grid.offsetLeft || top + HALF_UNIT_IN_PX < grid.offsetTop || left + tile.offsetWidth - HALF_UNIT_IN_PX > grid.offsetLeft + grid.offsetWidth || top + tile.offsetHeight - HALF_UNIT_IN_PX > grid.offsetTop + grid.offsetHeight);
+    }
 }
 
 function giveCoords(tile) {
     const tileHeight = parseInt(tile.querySelector(".row").childElementCount);
     const x = Math.round((tile.offsetLeft - grid.offsetLeft) / UNIT_IN_PX);
     const y = gridSize.height - Math.round((tile.offsetTop - grid.offsetTop) / UNIT_IN_PX) - tileHeight;
-    tile.setAttribute("anchor-x", x);
-    tile.setAttribute("anchor-y", y);
+    if (isMagnetMode) {
+        class Direction {
+            constructor(dir, dis, tile) {
+                this.dir = dir;
+                this.dis = dis;
+                this.tile = tile;
+            }
+        }
+        const directions = [
+            new Direction([-1, 0], null, null), // left
+            new Direction([0, -1], null, null), // top
+            new Direction([+1, 0], null, null), // right
+            new Direction([0, +1], null, null), // bottom
+        ];
+        const left = tile.offsetLeft + tile.offsetWidth / 2;
+        const top = tile.offsetTop + tile.offsetHeight / 2;
+        const width = tile.offsetWidth * UNIT_IN_PX;
+        const height = tile.offsetHeight * UNIT_IN_PX;
+        directions.forEach((item, idx) => {
+            switch (idx) {
+                case (0):
+                    for (let i = 0; i <= x; i++) {
+                        const temp = Object.values(document.elementsFromPoint(left - window.pageXOffset - i * UNIT_IN_PX, top - window.pageYOffset)).filter(item2 =>
+                            item2.classList.contains("tile") && item2.getAttribute("tile-index") !== tile.getAttribute("tile-index"));
+                        if (temp.length !== 0) {
+                            item.dis = tile.offsetLeft - temp[0].offsetLeft + temp[0].offsetWidth;
+                            item.tile = temp[0];
+                            break;
+                        }
+                    }
+                    break;
+                case (1):
+                    for (let i = 0; i <= gridSize.height - y; i++) {
+                        const temp = Object.values(document.elementsFromPoint(left - window.pageXOffset, top - window.pageYOffset - i * UNIT_IN_PX)).filter(item2 =>
+                            item2.classList.contains("tile") && item2.getAttribute("tile-index") !== tile.getAttribute("tile-index"));
+                        if (temp.length !== 0) {
+                            item.dis = tile.offsetTop - temp[0].offsetTop - temp[0].offsetHeight;
+                            item.tile = temp[0];
+                            break;
+                        }
+                    }
+                    break;
+                case (2):
+                    for (let i = 0; i <= gridSize.width - x; i++) {
+                        const temp = Object.values(document.elementsFromPoint(left - window.pageXOffset + i * UNIT_IN_PX, top - window.pageYOffset)).filter(item2 =>
+                            item2.classList.contains("tile") && item2.getAttribute("tile-index") !== tile.getAttribute("tile-index"));
+                        if (temp.length !== 0) {
+                            item.dis = temp[0].offsetLeft - tile.offsetLeft - tile.offsetWidth;
+                            item.tile = temp[0];
+                            break;
+                        }
+                    }
+                    break;
+                case (3):
+                    for (let i = 0; i <= y; i++) {
+                        const temp = Object.values(document.elementsFromPoint(left - window.pageXOffset, top - window.pageYOffset + i * UNIT_IN_PX)).filter(item2 =>
+                            item2.classList.contains("tile") && item2.getAttribute("tile-index") !== tile.getAttribute("tile-index"));
+                        if (temp.length !== 0) {
+                            item.dis = temp[0].offsetTop - tile.offsetTop - tile.offsetHeight;
+                            item.tile = temp[0];
+                            break;
+                        }
+                    }
+                    break;
+            }
+            console.log(directions);
+        })
+    } else {
+        tile.setAttribute("anchor-x", x);
+        tile.setAttribute("anchor-y", y);
+    }
 }
 
 function anchorTile(tile) {
@@ -134,13 +221,11 @@ function createTile() {
         tile.style.top = (touchLocation.pageY - tileOffset.y) + 'px';
     });
     tile.addEventListener("touchend", event => {
-        console.log(event);
         const tile = event.target;
-        if (isInGrid(tile)){
+        if (isInGrid(tile)) {
             giveCoords(tile);
             anchorTile(tile);
-        }
-        else tile.remove();
+        } else tile.remove();
         if (parseInt(tile.attributes["tile-index"].value) === tileCount - 1) createTile();
     });
     tileCount++;
@@ -172,12 +257,16 @@ tileHeight.addEventListener("input", event => {
     anchorTile(newTile);
 });
 
-const gridOnStyle = document.createElement("style");
-document.head.append(gridOnStyle);
-gridOn.addEventListener("click", event => {
-    isGridOn = gridOn.checked;
-    gridOnStyle.innerHTML = `.row, .col {visibility: ${isGridOn ? "visible" : "hidden"}}`;
-})
+const gridModeStyle = document.createElement("style");
+document.head.append(gridModeStyle);
+gridMode.addEventListener("click", event => {
+    isGridMode = gridMode.checked;
+    gridModeStyle.innerHTML = `.row, .col {visibility: ${isGridMode ? "visible" : "hidden"}}`;
+});
+
+magnetMode.addEventListener("click", event => {
+    isMagnetMode = magnetMode.checked;
+});
 
 // initiate
 
